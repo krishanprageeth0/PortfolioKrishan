abf5dc10238c54a2900abcc0736fce902fb1b7d5
krishanprageeth0 <Krishanprageeth87@gmail.com>
2024-03-16 00:02:14 +0530

    Added new files

---
+++ Script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer" , "Graphic Designer" , "Web Developer"] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+
+});
+
+
+
+
